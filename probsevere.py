de1b91c2838ed8299ed744da30e488aa6cd84e5e
jason leaver <jason.leaver.developer@gmail.com>
2021-10-15 21:07:13 +0000

    updatedTest

--- probsevere.py
+++ probsevere.py
@@ -1,27 +1,34 @@
 import numpy as np
-# from pprint import pprint
+from sklearn.metrics.pairwise import haversine_distances
+
+
 VERSION = '3.0.1'
 TYPE = 'FeatureCollection'
 PRODUCT = "probSevere:vectors"
 SOURCE = "NOAA/NCEP Central Operations"
-PRINT_DIV ='----------------\n'
-PRINT_BR ='\n################\n'
-R = 6378137
+PRINT_DIV = '----------------\n'
+PRINT_BR = '\n################\n'
+# 6.3710 * 100**3
+R = 6_371_000  # earth raidus in meters
+
+
 class StormCache(object):
     def __init__(self, my_dict):
         for key in my_dict:
             setattr(self, key, my_dict[key])
     # def
-  
+
+
 class NewCache():
-    motion=None
-    center=None
-    slope=None
-    storm_motion=None
+    motion = None
+    center = None
+    slope = None
+    storm_motion = None
 
 
-    
-nc=NewCache()
+nc = NewCache()
+
+
 class StormVectors:
     """
     StormTrackVector maintains a persistent cachry state to itterate over previous storm
@@ -29,117 +36,175 @@ class StormVectors:
     """
     _sv = {}
     count = 0
-    def _motion_slope(self,me,ms):
-        a = np.array([me,ms],dtype=np.float32)
-        _motion = np.multiply(a,[1,-1],dtype=np.float32)
-        _slope=np.divide(*_motion[[1,0]])
-        return(_motion,_slope)
 
-    def set_vector(self, valid_time=None,_id=None, center=None, motion_east=None, motion_south=None):
-        
+    def _motion_slope(self, me, ms):
+        a = np.array([me, ms], dtype=np.float32)
+        _motion = np.multiply(a, [1, -1], dtype=np.float32)
+        _slope = np.divide(*_motion[[1, 0]])
+        return(_motion, _slope)
+
+    def set_vector(self, valid_time=None, _id=None, center=None, motion_east=None, motion_south=None):
+
         self._id = _id
-        self._center = center
-        motion,slope=self._motion_slope(motion_east,motion_south)
+        self._center = np.array(center)
+        motion, slope = self._motion_slope(motion_east, motion_south)
 
         if _id not in self._sv.keys():
+            base_motion_vector = self._storm_motion_vector(motion)
             self._sv[_id] = {
-                "motion":motion,#self._motion,# np.array(self.motion),
-                "initial_center":center,
-                "center": center,
+                "motion": motion,  # self._motion,# np.array(self.motion),
+                "initial_center": np.array(center),
+                "center": np.array(center),
                 "slope": slope,
-                "storm_motion": self._mps_correction(motion),
-                "storm_prog":None
+                "base_motion_vector": base_motion_vector,
+                "mean_base_vector": base_motion_vector,
+                "haversine_vector": None,
+                "mean_haversine": None,
+                "mean_vector": None
             }
+
         else:
             # The stormcache class provides an api for the previous storm
             sc = StormCache(self._sv[_id])
             # storm motion forecast
-            sm_fcst=sc.storm_motion[0]
-            # forecast err
-            
-            
-            dM_dT = np.rollaxis(np.array([sc.motion,motion]), 1, 0)
-            prog_diff =np.diff([sc.center,center],1,0)
-            storm_prog=np.add(center,prog_diff)
-            storm_motion=self._mps_correction(motion)
-            # determine err
-            sm_err = np.diff([sc.storm_motion[0],center],axis=0)
-            # sp_err = np.diff([sc.storm_prog,center],axis=0)
+            # sm_fcst = sc.storm_motion[0]
+            # dM_dT = np.rollaxis(np.array([sc.motion, motion]), 1, 0)
+            # prog_diff = np.diff([sc.center, center], axis=0)
+            # print('diff:', np.diff([sc.center, center], axis=0), prog_diff)
+            # print(f'\nstorm_motion{motion}prevcenter{sc.center}center{center}')
+            #! MOTION VECTOR PROCESSING
+            # ? X_MOTION = SOME.FUNC()
+            # ? X_VECTOR= self._storm_motion_vector(X_MOTION)
+            #! X_MOTIONS
 
+            haversine_motion = self._haversine_mps(sc.center, center)
+            #!X_VECTORS
+            # ? BASE
+            base_motion_vector = self._storm_motion_vector(motion)
+            # ? BASE_MEAN
+            mean_base_vector = np.mean(
+                [base_motion_vector, sc.mean_base_vector], axis=0)
+            # ? HAVERSINE
+            haversine_vector = self._storm_motion_vector(
+                haversine_motion)
+            # ? CRUDE_PROG
 
-      
+            # crude_offset = np.array([0.60, 4.50, 9.00, 13.50, 18.00])
+            # prog_diff = np.diff([sc.center, center], 1, 0)
+            # crude_vector = np.array([center+np.multiply(prog_diff, x)
+            #                          for x in np.nditer(crude_offset)]).reshape((5, 2))
+            # print('crude vector', crude_vector)
 
+            # prog_diff = np.diff([sc.center, center], axis=0)
+            # prog_diff*4.5
+            # crude_vector = np.multiply(haversine_vector)
+            # try:
+            #     mean_haversine = np.mean(
+            #         [haversine_vector, sc.haversine_vector], axis=0)
 
+            # except:
+            if sc.haversine_vector is None:
+                mean_haversine = haversine_vector
+            else:
+                mean_haversine = np.mean(
+                    [haversine_vector, sc.mean_haversine], axis=0)
+                print(
+                    f"\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nhaversine_vector:\n{haversine_vector}\nsc.haversine_vector:\n{sc.haversine_vector}\nmean_haversine:\n{mean_haversine}\n")
+                # print(f"\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\n", haversine_vector, sc.haversine_vector, "!!!!!!!!!!!!!!!!!!!!!!!!!!", np.mean(
+                #     [haversine_vector, sc.haversine_vector], axis=0), '\n\n')
+                # mean_haversine = haversine_vector
 
+            mean_vector = np.mean(
+                [mean_base_vector, mean_haversine], axis=0)
+
+            # storm_prog = self._storm_prog_vector(prog_diff)
+
+            # print(f'prog_diff{prog_diff*4.5}')
+            # print(f'center:{center}')
+            # print(f'stormprog:{storm_prog}')
+
+            sm_err = np.diff([sc.base_motion_vector[0], center], axis=0)
+            try:
+                sp_err = np.diff([*sc.storm_prog, center], axis=0)
+                # print("$$$$$$$$$$$$$$$$$")
+                # print(f'prev{sc.center}\ncurr{center}')
+
+                # print("$$$$$$$$$$$$$$$$$")
+            except:
+                sp_err = None
+            # prog_err
+            # print('diff:', np.diff([sc.center, center], axis=0), )
             # change in movement change in time
-            print('STORM HISTORY LOG: \nunits:POS:[Longitude, latitude]\n')
-            print('STORM ID:', _id, '\nvalidtime:',valid_time)
-            print('INITIAL POS:',sc.initial_center )
-            # PREVIOUS STORM
-            print('\nPREVIOUS STORM INFO:')
-            print('POSITION:   ',sc.center )
-            print('SM_FCST POS:',sc.storm_motion[0] )
-            print('SP_FCST POS:',sc.storm_prog )
-            #CURRENT STORM
-            print('\nCURRENT STORM INFO:')
-            print('POSITION:   ',center)
-            print('SM_FCST POS:',storm_motion[0] )
-            print('SP_FCST POS:',*storm_prog )
-            print('SM_FCST ERR:',fcst_err)
-            print('STMPRG FCST:',storm_prog)
-            
-            # print()
-            ##############################
+            if False:
+                print('base_motion_vector:\n', base_motion_vector)
+                print('mean_base_vector:\n', mean_base_vector)
+                print('haversine_vector:\n', haversine_vector)
+                print('mean_haversine:\n', mean_haversine)
+                print('mean_vector:\n', mean_vector)
+                print('\n#################################\n')
+                print(
+                    'STORM HISTORY LOG: \nUNITS:\n pos:[longitude, latitude]\n')
+                print(' ')
+                print('#################################\n')
+                print('STORM ID:', _id, '\nvalidtime:', valid_time)
+                print('INITIAL POS:', sc.initial_center)
+                # PREVIOUS STORM
+                print('\nPREVIOUS STORM INFO:')
+                print('POSITION:   ', sc.center)
 
-    
-            print(f'{PRINT_DIV}[[mpsE,mpsN]#previous\n [mpsE,mpsN]]#current')
-            print(dM_dT)
-            # # storm motion change
-            print('2 min change in motion_vector:mps')
-            print(np.diff(dM_dT).reshape(1,2))
-            print(PRINT_BR)
-            print()
-            # print('change!!',self._storm_prog(sc.center,center),'\n',)
+                print('SM_FCST POS:', storm_motion)
+                print('SM_FCST ERR:', sm_err)
+                print('SP_FCST POS:', storm_prog)
+                print('SP_FCST ERR:', sp_err)
+
+                print('\nCURRENT STORM INFO:')
+                print('POSITION:   ', np.array(center))
 
             self._sv[_id] = {
                 "motion": motion,
                 "initial_center": sc.initial_center,
-                "center": center,
+                "center": np.array(center),
                 "slope": slope,
-                "storm_motion": storm_motion,
-                "storm_prog":storm_prog
+                "base_motion_vector": base_motion_vector,
+                "mean_base_vector": mean_base_vector,
+                "haversine_vector": haversine_motion,
+                "mean_haversine": mean_haversine,
+                "mean_vector": mean_vector
             }
     # GET THE DISTANCE BETWEEN TWO POINTS
-    def _crds_distance(self,crds1,crds2):
-        lat1 = np.radians(crds1[0])
-        lon1 = np.radians(crds1[1])
-        lat2 = np.radians(crds2[0])
-        lon2 = np.radians(crds2[1])
 
+    def _haversine_mps(self, prev_center, center):
+        # converting degs to rads
+        lon1, lat1, lon2, lat2 = np.deg2rad(
+            [*prev_center, *center], dtype=np.float32)
+        # haversine distance storm moved south
+        dSouth = haversine_distances(
+            [[lat1, lon1], [lat2, lon1]])
+        # haversine distance storm moved east
+        dEast = haversine_distances(
+            [[lat1, lon1], [lat1, lon2]])
+        # correction for movment
+        x = 1 if lon1 > lon2 else -1
+        y = 1 if lat1 > lat2 else -1
+        # returns motion south multiplied by the earths radius over time
+        return np.multiply([dEast[0, 1]*x, dSouth[0, 1]*y], 637_100/120)
 
-        dlon = lon2 - lon1
-        dlat = lat2 - lat1
+    def _storm_prog_vector(self, diff):
+        return np.add(self._center, diff)
 
-        a = np.sin(dlat / 2)**2 + np.cos(lat1) * np.cos(lat2) * np.sin(dlon / 2)**2
-        c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1 - a))
-
-        distance = R * c
-        return distance
-        print("Result:", distance)
-        print("Should be:", 278.546, "km")
-
-
-    def _mps_correction(self,mps=None):
+    def _storm_motion_vector(self, mps=None):
+        print(mps)
         """
-        
-        """
-        time_offset = [120, 900, 1800, 2700, 3600]  # mps offset for 2,15,30,45,60
 
-        distances=[np.multiply(mps, x) for x in time_offset]
+        """
+        time_offset = [120, 900, 1800, 2700,
+                       3600]  # mps offset for 2,15,30,45,60
+
+        distances = [np.multiply(mps, x) for x in time_offset]
 
         lat, lon = self._center
         arr = []
-        
+
         # for dn, de in self._offsets():
         for dn, de in distances:
             # Coordinate offsets in radians
@@ -150,11 +215,18 @@ class StormVectors:
             latO = lat + dLat * 180/np.pi
             lonO = lon + dLon * 180/np.pi
             arr.append([latO, lonO])
-        return arr
+
+        return np.array(arr)
 
     def _multi_line_string(self):
         mls = self._sv[self._id]
-        return [mls['storm_motion']]
+        return [
+            mls['base_motion_vector'],
+            mls['mean_base_vector'],
+            mls['haversine_vector'],
+            mls['mean_haversine'],
+            mls['mean_vector']
+        ]
 
 
 sv = StormVectors()
@@ -167,15 +239,14 @@ class ProbSevere:
     """
     features = []
 
-    def __init__(self, feature_collection=None,valid_time=None, features=None):
+    def __init__(self, feature_collection=None, valid_time=None, features=None):
         if feature_collection is not None:
-            _features =  feature_collection['features']
+            _features = feature_collection['features']
             self._valid_time = feature_collection['validTime']
         else:
             _features = features
-            self._valid_time= valid_time
+            self._valid_time = valid_time
 
-        # print(_valid_time,'\n')
         self.feature_collection = {
             'version': VERSION,
             'type': TYPE,
@@ -192,14 +263,18 @@ class ProbSevere:
         geometries = []
         # PROPS
         geom = feat['geometry']
+        coordinates = geom['coordinates']
         props = feat['properties']
         props['MODELS'] = feat['models']['probsevere']['LINE01']
+        # coordinates = geom['coordinates']
         # CRDS
-        lons, lats = np.rollaxis(np.array(geom['coordinates']), 2, 0)
+        # lons, lats = np.rollaxis(np.array(geom['coordinates']), 2, 0)
         geometries.append(geom)
 
         ################################|   MEAN CENTER    |############################################
-        center = [np.mean(lons), np.mean(lats)]
+
+        center = np.mean(*coordinates, axis=0)
+
         geometries.append({
             'type': 'point',
             'coordinates': center
@@ -210,7 +285,7 @@ class ProbSevere:
         mtn_s = props['MOTION_SOUTH']
         mtn_e = props['MOTION_EAST']
         sv.set_vector(_id=storm_id, motion_east=mtn_e, valid_time=self._valid_time,
-                       motion_south=mtn_s, center=center)
+                      motion_south=mtn_s, center=center)
 
         geometries.append({
             'type': 'MultiLineString',
--- test.py
+++ test.py
@@ -1,30 +1,90 @@
-from probsevere import ProbSevere
+# from probsevere import ProbSevere
 import json
 from pprint import pprint
 from glob import glob
 import os
 from pprint import pprint
 from types import SimpleNamespace
-
+# from sklearn.metrics.pairwise import haversine_distances
+import numpy as np
+from sklearn.metrics.pairwise import haversine_distances
 
 
 def load(feature):
     jd = json.dumps(feature)
-    print(jd)
+    # print(jd)
     return json.loads(jd, object_hook=lambda d: SimpleNamespace(**d))
 
 
-# ? schedule itterator to run every 8 mins
+        # arr.append(geometry)
+# array =[[1,2,3],[1,2,3]]
+
+
+# print(array[0][0])
+class Storms:
+    _storm = {}
+    def set(self,_id=None,coordinates=None,properties=None):
+        center = np.mean(np.squeeze(coordinates),axis=0)
+        self._id = _id
+
+        # self._id = self._storm[_id]
+
+        if _id not in self._storm.keys():
+            self._storm[_id]={
+                'coordinates':np.array(coordinates),
+                'center':np.array([center])#[center]
+            }
+        else:
+            self._storm[_id]['center'] = np.append(self._storm[_id]['center'],[center],axis=0)
+            rads = np.radians(self._storm[_id]['center'][-2:])
+            D2min=haversine_distances(rads)
+
+            self._storm[_id]['motion']=D2min* 6371000/1000 
+            
+
+
+       
+
+
+            return None
+    def getById(self,_id):
+        return self._storm[_id]
+
+stm = Storms()
+
+
+
+
+
+samples = [
+    # 'sample_data/MRMS_PROBSEVERE_20211011_000053.json',
+    'sample_data/MRMS_PROBSEVERE_20211011_000240.json',
+    'sample_data/MRMS_PROBSEVERE_20211011_000447.json',
+    'sample_data/MRMS_PROBSEVERE_20211011_000655.json'
+    ]
+
+
+
+
 def ready(featureCollection):
     for feature in featureCollection['features']:
-        print()
         geometry = load(feature['geometry'])
         properties = load(feature['properties'])
-        print(geometry.coordinates,'\n',properties.ID)
-        
+        stm.set(_id=properties.ID,coordinates=geometry.coordinates)
+
+for sample in samples:
+    ready(json.load(open(sample)))
+
+a = stm.getById('90077')
+print(a['motion'])
 
 
-ready(json.load(open('sample_data/MRMS_PROBSEVERE_20210925_144634.json')))
+
+
+
+
+
+
 
 def get_feats_by_id(features,_id):
     arr =[]
