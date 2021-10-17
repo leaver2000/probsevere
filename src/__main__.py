8a8eadd072cbb00576f35c06b1709d8f600fe8ac
Jason Leaver <jason.leaver.developer@gmail.com>
2021-10-17 06:54:22 -0500

    object list

--- src/__main__.py
+++ src/__main__.py
@@ -8,17 +8,17 @@ samples = [
     'sample_data/MRMS_PROBSEVERE_20211011_000240.json',
     'sample_data/MRMS_PROBSEVERE_20211011_000447.json',
     'sample_data/MRMS_PROBSEVERE_20211011_000655.json'
-    ]
+]
 
 
-keys =[
+keys = [
     'MUCAPE',
     'MLCAPE',
-    'MLCIN', 
-    'EBSHEAR',#EFFECTIVE BULK SHEAR
+    'MLCIN',
+    'EBSHEAR',  # EFFECTIVE BULK SHEAR
     'SRH01KM',
     'MEANWIND_1-3kmAGL',
-    'MESH',#maximum estimated size of hail
+    'MESH',  # maximum estimated size of hail
     'VIL_DENSITY',
     'FLASH_RATE',
     'FLASH_DENSITY',
@@ -28,16 +28,36 @@ keys =[
 ]
 
 
+class History:
+    tory = []
+
+    def __init__(self):
+        pass
+
+    def set(self, _id):
+        print(_id)
+
+
+hst = History()
 
 if __name__ == '__main__':
     # print('hello')
 
     for sample in samples:
-        feature = json.load(open(sample))
-        ps = ProbSevere(feature).storms
-        # ps.getCollection()
+        featureCollection = json.load(open(sample))
+        # print(feature['validTime'])
+        validTime = featureCollection['validTime']
+        for feature in featureCollection['features']:
+            _id = feature['properties']['ID']
+            coordinates = feature['geometry']['coordinates']
+            b = np.array(list(zip([_id], coordinates)))
+            # c = np.vstack(([(_id)], np.array(coordinates)))
+            # print(np.hstack((_id, [(2)])))
 
-    # pprint(ps.compareProps('90101'))
-    # pprint(ps.getCollection())
+            # print(x['_id'] == [_id])
+            x = np.array([(_id, validTime, np.array(coordinates, dtype='object'))],
+                         dtype=[('_id', 'U10'), ('validtime', 'U15'), ('coordinates', list)], )
 
-
+            # b = np.stack([[_id], coordinates], axis=0)
+            # # setattr(b, 'stuff', 'stff')
+            # print(b)
